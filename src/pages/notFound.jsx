b82437eb841fe88e6484c04c8cfc344026361dae
Dmitrii <dimterion59@gmail.com>
2025-12-16 10:45:39 +0100

    adjust notFound page

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -1,3 +1,15 @@
+import { Link } from "react-router";
+
 export default function NotFoundPage() {
-  return <div>Not Found Page</div>;
+  return (
+    <main className="relative flex flex-col items-center justify-center gap-10">
+      <h2 className="text-center text-4xl font-bold">Page not found</h2>
+      <Link
+        to="/"
+        className="border-3 px-6 py-2 text-lg transition-all hover:bg-[#fdf0d5] hover:text-[#003049]"
+      >
+        Home page
+      </Link>
+    </main>
+  );
 }
