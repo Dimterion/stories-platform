38ea92e71ab7347149897e1de6d8a9655ff2f560
Dmitrii <dimterion59@gmail.com>
2025-12-16 10:48:22 +0100

    adjust about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router";
+
 export default function AboutPage() {
-  return <div>About Page</div>;
+  return (
+    <main className="relative flex flex-col items-center justify-center gap-10">
+      <h2 className="text-center text-4xl font-bold">
+        Interactive Stories Platform
+      </h2>
+      <p>
+        A platform to create interactive stories with multiple choices and
+        outcomes.
+      </p>
+      <Link
+        to="/"
+        className="border-3 px-6 py-2 text-lg transition-all hover:bg-[#fdf0d5] hover:text-[#003049]"
+      >
+        Home page
+      </Link>
+    </main>
+  );
 }
