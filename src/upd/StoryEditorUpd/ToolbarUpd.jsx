491c06e1516dfee914b21fef0f09c2d41b0db959
Dmitrii <dimterion59@gmail.com>
2026-01-14 10:29:05 +0100

    adjust ToolbarUpd buttons style

--- src/upd/StoryEditorUpd/ToolbarUpd.jsx
+++ src/upd/StoryEditorUpd/ToolbarUpd.jsx
@@ -18,20 +18,20 @@ export default function Toolbar({
     <section className="flex flex-wrap justify-center gap-4 pb-2 md:justify-start">
       <button
         onClick={onExportStory}
-        className="border-darkBlue bg-baseOrange hover:bg-lightOrange inline-flex w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center"
+        className="border-darkBlue bg-baseOrange hover:bg-lightOrange inline-flex min-h-20 w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center text-sm sm:text-base"
       >
-        <FileUp />
+        <FileUp className="size-8 sm:size-6" />
         Export Story (JSON file)
       </button>
       <button
         onClick={onExportHTML}
-        className="border-darkBlue bg-lightGreen hover:bg-darkGreen inline-flex w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center"
+        className="border-darkBlue bg-lightGreen hover:bg-darkGreen inline-flex min-h-20 w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center text-sm sm:text-base"
       >
-        <FileUp />
+        <FileUp className="size-8 sm:size-6" />
         Export Story (HTML file)
       </button>
-      <label className="border-darkBlue bg-darkGreen hover:bg-lightGreen inline-flex w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center">
-        <FileDown />
+      <label className="border-darkBlue bg-darkGreen hover:bg-lightGreen inline-flex min-h-20 w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center text-sm sm:text-base">
+        <FileDown className="size-8 sm:size-6" />
         Import Story (JSON file)
         <input
           type="file"
@@ -42,23 +42,23 @@ export default function Toolbar({
       </label>
       <button
         onClick={onShowDiagram}
-        className="border-darkBlue bg-basePurple hover:bg-lightPurple inline-flex w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center"
+        className="border-darkBlue bg-basePurple hover:bg-lightPurple inline-flex min-h-20 w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center text-sm sm:text-base"
       >
-        <Map />
+        <Map className="size-8 sm:size-6" />
         View Story Diagram
       </button>
       <button
         onClick={onClearSave}
-        className="border-darkBlue bg-baseRed hover:bg-lightRed inline-flex w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center"
+        className="border-darkBlue bg-baseRed hover:bg-lightRed inline-flex min-h-20 w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center text-sm sm:text-base"
       >
-        <XCircle />
+        <XCircle className="size-8 sm:size-6" />
         Clear Save & Reset Editor
       </button>
       <button
         onClick={onShowModal}
-        className="border-darkBlue bg-baseGreen hover:bg-softGreen inline-flex w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center"
+        className="border-darkBlue bg-baseGreen hover:bg-softGreen inline-flex min-h-20 w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center text-sm sm:text-base"
       >
-        <CircleQuestionMark />
+        <CircleQuestionMark className="size-8 sm:size-6" />
         Instructions
       </button>
     </section>
