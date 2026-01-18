67e60479d314733dd3f082645eb31770c6ecdc2c
Dmitrii <dimterion59@gmail.com>
2026-01-18 12:33:39 +0100

    adjust home page buttons & icons

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router";
+import { BookOpenCheck, CircleQuestionMark, Pencil } from "lucide-react";
 import Modal from "../components/Modal";
 import Instructions from "../components/Instructions";
 
@@ -14,9 +15,9 @@ export default function HomePage() {
       <button
         type="button"
         onClick={() => setShowModal(true)}
-        className="hover:bg-softWhite hover:text-deepBlue mx-auto w-sm max-w-[80vw] cursor-pointer border p-2 text-center uppercase"
+        className="hover:bg-softWhite hover:text-deepBlue mx-auto flex w-sm max-w-[80vw] cursor-pointer flex-nowrap items-center justify-center gap-2 border p-2 text-center uppercase"
       >
-        Instructions
+        <CircleQuestionMark /> Instructions
       </button>
       <section className="flex flex-col gap-10 text-center">
         <article className="flex flex-col gap-4">
@@ -26,18 +27,20 @@ export default function HomePage() {
           </p>
           <Link
             to="/story-player"
-            className="hover:bg-softWhite hover:text-deepBlue mx-auto w-sm max-w-[80vw] border p-2 text-center uppercase"
+            className="hover:bg-softWhite hover:text-deepBlue mx-auto flex w-sm max-w-[80vw] flex-nowrap items-center justify-center gap-2 border p-2 text-center uppercase"
           >
-            Play Stories
+            <BookOpenCheck />
+            Story Player
           </Link>
         </article>
         <article className="flex flex-col gap-4">
           <p>Create your own interactive stories.</p>
           <Link
             to="/story-editor"
-            className="hover:bg-softWhite hover:text-deepBlue mx-auto w-sm max-w-[80vw] border p-2 text-center uppercase"
+            className="hover:bg-softWhite hover:text-deepBlue mx-auto flex w-sm max-w-[80vw] flex-nowrap items-center justify-center gap-2 border p-2 text-center uppercase"
           >
-            Create Stories
+            <Pencil />
+            Story Editor
           </Link>
         </article>
         <article className="flex flex-col gap-4">
