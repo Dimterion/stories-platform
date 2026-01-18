05d603151afd12e4ffbbd81b8128fa8a6d6baaf4
Dmitrii <dimterion59@gmail.com>
2026-01-18 12:28:48 +0100

    adjust notFound page

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router";
+import { House } from "lucide-react";
 
 export default function NotFoundPage() {
   return (
-    <main className="relative flex flex-col items-center justify-center gap-10">
+    <main className="relative flex flex-col items-center justify-center gap-10 p-2">
       <h2 className="text-center text-4xl font-bold">Page not found</h2>
+      <p className="text-center text-lg">
+        Please check the link to the page or try refreshing it.
+      </p>
       <Link
         to="/"
-        className="hover:bg-softWhite hover:text-deepBlue border-3 px-6 py-2 text-lg transition-all"
+        className="hover:bg-softWhite hover:text-deepBlue flex flex-nowrap items-center gap-2 border-3 px-6 py-2 text-lg uppercase transition-all"
       >
-        Home page
+        <House /> Home page
       </Link>
     </main>
   );
