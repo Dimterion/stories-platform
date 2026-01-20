7bb61479a7579780dcd2bf8ee0647b8271f18ce8
Dmitrii <dimterion59@gmail.com>
2026-01-20 10:14:09 +0100

    update home page layout

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -20,39 +20,35 @@ export default function HomePage() {
         <CircleQuestionMark /> Instructions
       </button>
       <section className="flex flex-col gap-10 text-center">
-        <article className="flex flex-col gap-4">
-          <p className="flex gap-2">
-            Read / play through interactive stories with multiple choices and
-            outcomes.
-          </p>
-          <Link
-            to="/story-player"
-            className="hover:bg-softWhite hover:text-deepBlue mx-auto flex w-sm max-w-[80vw] flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase sm:text-base"
-          >
-            <BookOpenCheck />
-            Story Player
-          </Link>
-        </article>
-        <article className="flex flex-col gap-4">
-          <p>Create your own interactive stories.</p>
-          <Link
-            to="/story-editor"
-            className="hover:bg-softWhite hover:text-deepBlue mx-auto flex w-sm max-w-[80vw] flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase sm:text-base"
-          >
-            <Pencil />
-            Story Editor
-          </Link>
-        </article>
-        <article className="flex flex-col gap-4">
-          <p>Learn more about the platform.</p>
-          <Link
-            to="/about"
-            className="hover:bg-softWhite hover:text-deepBlue mx-auto flex w-sm max-w-[80vw] flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase sm:text-base"
-          >
-            <Info />
-            About the platform
-          </Link>
-        </article>
+        <div className="flex flex-wrap gap-4">
+          <article className="m-auto flex w-sm max-w-[80vw] flex-col gap-4 border p-2">
+            <Link
+              to="/story-player"
+              className="hover:bg-softWhite hover:text-deepBlue mx-auto flex w-full flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase sm:text-base"
+            >
+              <BookOpenCheck />
+              Story Player
+            </Link>
+            <p className="text-sm">Read / play through interactive stories.</p>
+          </article>
+          <article className="m-auto flex w-sm max-w-[80vw] flex-col gap-4 border p-2">
+            <Link
+              to="/story-editor"
+              className="hover:bg-softWhite hover:text-deepBlue mx-auto flex w-full flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase sm:text-base"
+            >
+              <Pencil />
+              Story Editor
+            </Link>
+            <p className="text-sm">Create your own interactive stories.</p>
+          </article>
+        </div>
+        <Link
+          to="/about"
+          className="hover:bg-softWhite hover:text-deepBlue mx-auto flex w-sm max-w-[80vw] flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase sm:text-base"
+        >
+          <Info />
+          About the platform
+        </Link>
       </section>
       <Modal
         isOpen={showModal}
