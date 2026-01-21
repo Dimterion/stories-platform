8b3b8f074bf6e1cef196f0d7abf53bb13c2cecaf
Dmitrii <dimterion59@gmail.com>
2026-01-21 15:08:53 +0100

    adjust home page elements

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,48 +8,48 @@ export default function HomePage() {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <main className="flex flex-col items-center gap-10 p-4">
-      <h1 className="text-center text-4xl italic">
+    <main className="flex flex-col items-center gap-12 p-4">
+      <h1 className="my-2 text-center text-4xl italic">
         Interactive Stories Platform
       </h1>
       <button
         type="button"
         onClick={() => setShowModal(true)}
-        className="hover:bg-softWhite hover:text-deepBlue bg-darkBlue mx-auto flex w-sm max-w-[80vw] cursor-pointer flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase sm:text-base"
+        className="hover:bg-softWhite hover:text-deepBlue bg-darkBlue mx-auto flex w-sm max-w-[80vw] cursor-pointer flex-nowrap items-center justify-center gap-2 border p-2 text-center uppercase"
       >
         <CircleQuestionMark /> Instructions
       </button>
       <section className="flex flex-col gap-10 text-center">
         <div className="flex flex-wrap gap-4">
-          <article className="bg-darkBlue m-auto flex w-sm max-w-[80vw] flex-col gap-4 border p-2">
+          <article className="bg-darkBlue m-auto flex w-sm max-w-[80vw] flex-col gap-4 border p-4">
             <Link
               to="/story-player"
-              className="hover:bg-softWhite hover:text-deepBlue mx-auto flex w-full flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase sm:text-base"
+              className="hover:bg-softWhite hover:text-deepBlue mx-auto flex w-full flex-nowrap items-center justify-center gap-2 border p-2 text-center uppercase"
             >
               <BookOpenCheck />
               Story Player
             </Link>
-            <p className="text-sm">Read / play through interactive stories.</p>
+            <p>Read / play interactive stories.</p>
           </article>
-          <article className="bg-darkBlue m-auto flex w-sm max-w-[80vw] flex-col gap-4 border p-2">
+          <article className="bg-darkBlue m-auto flex w-sm max-w-[80vw] flex-col gap-4 border p-4">
             <Link
               to="/story-editor"
-              className="hover:bg-softWhite hover:text-deepBlue mx-auto flex w-full flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase sm:text-base"
+              className="hover:bg-softWhite hover:text-deepBlue mx-auto flex w-full flex-nowrap items-center justify-center gap-2 border p-2 text-center uppercase"
             >
               <Pencil />
               Story Editor
             </Link>
-            <p className="text-sm">Create your own interactive stories.</p>
+            <p>Create your own interactive stories.</p>
           </article>
         </div>
-        <Link
-          to="/about"
-          className="hover:bg-softWhite hover:text-deepBlue bg-darkBlue mx-auto flex w-sm max-w-[80vw] flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase sm:text-base"
-        >
-          <Info />
-          About the platform
-        </Link>
       </section>
+      <Link
+        to="/about"
+        className="hover:bg-softWhite hover:text-deepBlue bg-darkBlue mx-auto flex w-sm max-w-[80vw] flex-nowrap items-center justify-center gap-2 border p-2 text-center uppercase"
+      >
+        <Info />
+        About the platform
+      </Link>
       <Modal
         isOpen={showModal}
         onClose={() => setShowModal(false)}
