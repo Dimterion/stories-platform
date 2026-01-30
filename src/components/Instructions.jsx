fafcc1af3b44308ffe8bcbf7a4beaef841192738
Dmitrii <dimterion59@gmail.com>
2026-01-30 11:53:35 +0100

    add conditional logic to Instructions component to display pages content appropriately

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,52 +1,61 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import { ExternalLink } from "lucide-react";
 
 export default function Instructions() {
+  const location = useLocation();
+  const path = location.pathname;
+  const isStoryPlayer = path.startsWith("/story-player");
+  const isStoryEditor = path.startsWith("/story-editor");
+
   return (
     <section className="space-y-4">
       <h3 className="text-center text-xl font-bold">Instructions</h3>
-      <article>
-        <p>
-          Use{" "}
-          <Link
-            to="/story-player"
-            className="hover:text-lightBlue font-bold underline"
-          >
-            Story Player
-          </Link>{" "}
-          to read/play through the interactive stories.
-        </p>
-        <ul className="list-disc space-y-1 pt-2 pl-5">
-          <li>Upload your own stories in JSON format.</li>
-          <li>Navigate through the stories by choosing various options.</li>
-          <li>Progress is saved locally in your current browser.</li>
-          <li>Clear and reset progress to start over.</li>
-          <li>Use "Show Hints" button to view additional instructions.</li>
-        </ul>
-      </article>
-      <article>
-        <p>
-          Use{" "}
-          <Link
-            to="/story-editor"
-            className="hover:text-lightBlue font-bold underline"
-          >
-            Story Editor
-          </Link>{" "}
-          to create interactive stories.
-        </p>
-        <ul className="list-disc space-y-1 pt-2 pl-5">
-          <li>Upload your own stories in JSON format.</li>
-          <li>Download created stories in HTML or JSON format.</li>
-          <li>Progress is saved locally in your current browser.</li>
-          <li>Clear and reset progress to start over.</li>
-          <li>
-            Review your stories structure by using{" "}
-            <strong>Story diagram</strong> functionality.
-          </li>
-          <li>Download your stories diagrams in SVG or PNG format.</li>
-        </ul>
-      </article>
+      {!isStoryEditor && (
+        <article>
+          <p>
+            Use{" "}
+            <Link
+              to="/story-player"
+              className="hover:text-lightBlue font-bold underline"
+            >
+              Story Player
+            </Link>{" "}
+            to read/play through the interactive stories.
+          </p>
+          <ul className="list-disc space-y-1 pt-2 pl-5">
+            <li>Upload your own stories in JSON format.</li>
+            <li>Navigate through the stories by choosing various options.</li>
+            <li>Progress is saved locally in your current browser.</li>
+            <li>Clear and reset progress to start over.</li>
+            <li>Use "Show Hints" button to view additional instructions.</li>
+          </ul>
+        </article>
+      )}
+      {!isStoryPlayer && (
+        <article>
+          <p>
+            Use{" "}
+            <Link
+              to="/story-editor"
+              className="hover:text-lightBlue font-bold underline"
+            >
+              Story Editor
+            </Link>{" "}
+            to create interactive stories.
+          </p>
+          <ul className="list-disc space-y-1 pt-2 pl-5">
+            <li>Upload your own stories in JSON format.</li>
+            <li>Download created stories in HTML or JSON format.</li>
+            <li>Progress is saved locally in your current browser.</li>
+            <li>Clear and reset progress to start over.</li>
+            <li>
+              Review your stories structure by using{" "}
+              <strong>Story diagram</strong> functionality.
+            </li>
+            <li>Download your stories diagrams in SVG or PNG format.</li>
+          </ul>
+        </article>
+      )}
       <article>
         <p>
           Visit the{" "}
