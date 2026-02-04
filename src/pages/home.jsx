f265bfedd6383a42a8a555a4fb269a18d6c1625d
Dmitrii <dimterion59@gmail.com>
2026-02-04 16:26:17 +0100

    add Home page animations

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,36 +9,34 @@ export default function HomePage() {
 
   return (
     <section className="mx-auto mb-4 flex max-w-[1024px] flex-col items-center justify-center gap-6 p-4 sm:mb-0 sm:gap-12">
-      <h2 className="my-2 text-center text-2xl font-bold italic sm:text-4xl">
+      <h2 className="homePage-h2 my-2 text-center text-2xl font-bold italic opacity-0 sm:text-4xl">
         Interactive Stories Platform
       </h2>
       <button
         type="button"
         onClick={() => setShowModal(true)}
-        className="hover:bg-softWhite hover:text-deepBlue bg-darkBlue mx-auto flex w-sm max-w-[80vw] cursor-pointer flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase sm:text-base"
+        className="homePage-btn hover:bg-softWhite hover:text-deepBlue bg-darkBlue mx-auto flex w-sm max-w-[80vw] cursor-pointer flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase opacity-0 sm:text-base"
       >
         <CircleQuestionMark /> Instructions
       </button>
       <section className="flex flex-wrap gap-4 text-center">
-        <article className="bg-darkBlue m-auto flex w-sm max-w-[80vw] flex-col gap-4 border p-4">
+        <article className="homePage-leftLink bg-darkBlue m-auto flex w-sm max-w-[80vw] flex-col gap-4 border p-4 opacity-0">
           <Link
             to="/story-player"
             className="hover:bg-softWhite hover:text-deepBlue mx-auto flex w-full flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase sm:text-base"
           >
-            <BookOpenCheck />
-            Story Player
+            <BookOpenCheck /> Story Player
           </Link>
           <p className="text-sm sm:text-base">
             Read / play interactive stories.
           </p>
         </article>
-        <article className="bg-darkBlue m-auto flex w-sm max-w-[80vw] flex-col gap-4 border p-4">
+        <article className="homePage-rightLink bg-darkBlue m-auto flex w-sm max-w-[80vw] flex-col gap-4 border p-4 opacity-0">
           <Link
             to="/story-editor"
             className="hover:bg-softWhite hover:text-deepBlue mx-auto flex w-full flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase sm:text-base"
           >
-            <Pencil />
-            Story Editor
+            <Pencil /> Story Editor
           </Link>
           <p className="text-sm sm:text-base">
             Create your own interactive stories.
@@ -47,10 +45,9 @@ export default function HomePage() {
       </section>
       <Link
         to="/about"
-        className="hover:bg-softWhite hover:text-deepBlue bg-darkBlue mx-auto flex w-sm max-w-[80vw] flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase sm:text-base"
+        className="homePage-bottomLink hover:bg-softWhite hover:text-deepBlue bg-darkBlue mx-auto flex w-sm max-w-[80vw] flex-nowrap items-center justify-center gap-2 border p-2 text-center text-xs uppercase opacity-0 sm:text-base"
       >
-        <Info />
-        About the platform
+        <Info /> About the platform
       </Link>
       <Modal
         isOpen={showModal}
