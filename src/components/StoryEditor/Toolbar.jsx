29529fb7a345a50173cc26c95315a47d0cdd2089
Dmitrii <dimterion59@gmail.com>
2026-02-09 11:35:53 +0100

    update Toolbar buttons

--- src/components/StoryEditor/Toolbar.jsx
+++ src/components/StoryEditor/Toolbar.jsx
@@ -26,13 +26,12 @@ export default function Toolbar({
         icon={<FileUp className="size-8 sm:size-6" />}
         text="Export Story (JSON file)"
       />
-      <button
+      <ToolbarBtn
         onClick={onExportHTML}
-        className="border-darkBlue bg-lightGreen hover:bg-darkGreen inline-flex min-h-20 w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center text-sm sm:text-base"
-      >
-        <FileUp className="size-8 sm:size-6" />
-        Export Story (HTML file)
-      </button>
+        color="bg-lightGreen hover:bg-darkGreen"
+        icon={<FileUp className="size-8 sm:size-6" />}
+        text="Export Story (HTML file)"
+      />
       <label className="border-darkBlue bg-darkGreen hover:bg-lightGreen inline-flex min-h-20 w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center text-sm sm:text-base">
         <FileDown className="size-8 sm:size-6" />
         Import Story (JSON file)
@@ -43,27 +42,24 @@ export default function Toolbar({
           className="hidden"
         />
       </label>
-      <button
+      <ToolbarBtn
         onClick={onShowDiagram}
-        className="border-darkBlue bg-basePurple hover:bg-lightPurple inline-flex min-h-20 w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center text-sm sm:text-base"
-      >
-        <Map className="size-8 sm:size-6" />
-        View Story Diagram
-      </button>
-      <button
+        color="bg-basePurple hover:bg-lightPurple"
+        icon={<Map className="size-8 sm:size-6" />}
+        text="View Story Diagram"
+      />
+      <ToolbarBtn
         onClick={onClearSave}
-        className="border-darkBlue bg-baseRed hover:bg-lightRed inline-flex min-h-20 w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center text-sm sm:text-base"
-      >
-        <XCircle className="size-8 sm:size-6" />
-        Clear Save & Reset Editor
-      </button>
-      <button
+        color="bg-baseRed hover:bg-lightRed"
+        icon={<XCircle className="size-8 sm:size-6" />}
+        text="Clear Save & Reset Editor"
+      />
+      <ToolbarBtn
         onClick={onShowModal}
-        className="border-darkBlue bg-baseGreen hover:bg-softGreen inline-flex min-h-20 w-3xs max-w-[55vw] cursor-pointer items-center gap-2 border-3 px-4 py-2 text-center text-sm sm:text-base"
-      >
-        <CircleQuestionMark className="size-8 sm:size-6" />
-        Instructions
-      </button>
+        color="bg-baseGreen hover:bg-softGreen"
+        icon={<CircleQuestionMark className="size-8 sm:size-6" />}
+        text="Instructions"
+      />
     </section>
   );
 }
