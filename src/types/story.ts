58f1082d0ae0afaec31d31f6eaebd08dcef5e13c
Dmitrii <dimterion59@gmail.com>
2026-02-24 10:17:01 +0100

    add story.ts file

---
+++ src/types/story.ts
@@ -0,0 +1,22 @@
+export type StoryOption = {
+  text: string;
+  next: string;
+  nextLabel?: string;
+};
+
+export type StoryNode = {
+  label: string;
+  text: string;
+  options: StoryOption[];
+  createdAt: number;
+};
+
+export type Story = {
+  title?: string;
+  author?: string;
+  description?: string;
+  start: string;
+  nodes: Record<string, StoryNode>;
+  showProgress?: boolean;
+  allowBackNavigation?: boolean;
+};
